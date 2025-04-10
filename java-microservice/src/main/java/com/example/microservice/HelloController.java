0d27b9fef69a8f20a89819d287cbf77227bfb20d
PRATHEEK U B <83898956+pratheek08@users.noreply.github.com>
2025-04-10 16:37:43 +0530

    Add files via upload

---
+++ java-microservice/src/main/java/com/example/microservice/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.microservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from the Java Microservice!";
+    }
+}
